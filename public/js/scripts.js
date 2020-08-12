ea71a9e7b25bab3ef064d5987b2e240885c3510b
technich214 <minhquangmori214@gmail.com>
2020-08-12 17:14:16 +0700

    Final Build Heroku

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,45 +2,88 @@ document.addEventListener("DOMContentLoaded", function (event) {
     var peer_id;
     var username;
     var conn;
+
+    var mediaOptions = {
+        video: true,
+        audio: true
+    };
+
     //Bug fixing
     navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || navigator.mediaDevices.webkitGetUserMedia || navigator.mediaDevices.mozGetUserMedia;//Browser patching
-    var peer = new Peer();
+
+    var peer = new Peer({
+        key: 'peerjs',
+        host: 'scpeerjsserver.herokuapp.com',
+        secure: true,
+        port: 443,
+        config: {
+            'iceServers': [
+                { url: 'stun:stun.l.google.com:19302' },
+                { url: 'stun:stun1.l.google.com:19302' },
+                { url: 'stun:stun2.l.google.com:19302' },
+                { url: 'stun:stun3.l.google.com:19302' },
+                { url: 'stun:stun4.l.google.com:19302' },
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'muazkh',
+                    username: 'webrtc@live.com'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=udp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                },
+                {
+                    url: 'turn:192.158.29.39:3478?transport=tcp',
+                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                    username: '28224511:1379330808'
+                }
+            ]
+        }
+    });
+
     var myVideo = document.getElementById('my-camera');
     var peerVideo = document.getElementById('peer-camera');
     var myStream;
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: true
-    }).then(function (stream) {
+    var peerStream;
+
+    //Getting local Stream
+    navigator.mediaDevices.getUserMedia(mediaOptions).then(function (stream) {
+        //Store in global refrence for later use
         myStream = stream;
         myVideo.srcObject = myStream;
     });
+
+    //Getting peer id
     peer.on('open', function (ShowId) {
         document.getElementById('peer-id-label').innerHTML = 'This is YOUR ID: ' + peer.id;
     });
 
+    //Establish connection
     peer.on('connection', function (connection) {
-        alert('Connected');
+        //Store in global refrence for later use
         conn = connection;
         peer_id = connection.peer;
+        var peerName = connection.metadata.username;
+        //Alert user upon connection
+        alert(peer_id + ' has connected to you!\nPress [Connect to peer] to open 2 way communication');
+        //Upon recieve data display it correctly
         conn.on('data', handleMessage);
         //Disapear peer_id and set incoming id as peer_id value
         document.getElementById('peer_id').className += ' invisible';
         document.getElementById('peer_id').value = peer_id;
-        document.getElementById('connected_peer').innerHTML = connection.metadata.username;
-        document.getElementById('connector_peer').className = 'badge badge-secondary';
-    });
-    //Reporting error
-    peer.on('error', function (err) {
-        alert('Something went wrong: ' + err);
-        console.error(err);
+        document.getElementById('connected_peer_container').className = 'col-lg-5 control-label text-danger';
+        //If peerName is Empty replace it with Anonymous
+        if (peerName) {
+            document.getElementById('connected_peer').innerHTML = peerName;
+        } else document.getElementById('connected_peer').innerHTML = 'Anonymous';
     });
 
     //Recieving end
     peer.on('call', function (call) {
-        alert('Incoming call')
-        //var acceptCall = confirm('Incoming Call. Accept ?');
-        //Answer call with your stream(video/audio)
+        console.log('Incoming call');
+        alert('Incoming call');
+        //Answer call with local stream
         call.answer(myStream);
         //Recive data;
         call.on('stream', function (stream) {
@@ -51,6 +94,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             alert('Call ended');
         });
     });
+
     //Add received and sent messages to chat box;
     function handleMessage(data) {
         var orientation = "text-left";
@@ -58,16 +102,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
         if (data.from == username) {
             orientation = 'text-right';
         }
-        var messageHTML = '<a href="javascript:void(0);" class="list-group-item' + orientation + '">';
-        messageHTML += '<h4 class="list-group-item-heading">' + data.from + '</h4>';
+        var messageHTML = '<a href="javascript:void(0);" class="list-group-item ' + orientation + '">';
+        if (data.from != '') {
+            messageHTML += '<h4 class="list-group-item-heading">' + data.from + '</h4>';
+        } else messageHTML += '<h4 class="list-group-item-heading">' + 'Anonymous' + '</h4>';
         messageHTML += '<p class="list-group-item-text">' + data.text + '</p>';
         messageHTML += '</a>';
         document.getElementById("messages").innerHTML += messageHTML;
     }
+
+    //Microphone toggle //NOT WORKING 
+    document.getElementById('micToggle').addEventListener('click', function () {
+        if (mediaOptions.audio == false) {
+            mediaOptions.audio = true;
+            document.getElementById('micToggle').innerHTML = 'Mic off';
+            console.log('Audio is on');
+        } else {
+            mediaOptions.audio = false;
+            document.getElementById('micToggle').innerHTML = 'Mic on';
+            console.log('Audio is off');
+        }
+    });
+
     //Send message button
     document.getElementById('send-message').addEventListener('click', function () {
         //Get text;
         var text = document.getElementById('message').value;
+        var messageContainer = document.getElementById('messages-container');
         //Preping data
         var data = {
             from: username,
@@ -75,37 +136,62 @@ document.addEventListener("DOMContentLoaded", function (event) {
         };
         //Peerjs send message
         conn.send(data);
-
+        //Display sended message on screen
         handleMessage(data);
-        document.getElementById("message").value = "";//Go down a line
+        //Go down a line
+        document.getElementById("message").value = "";
+        //Scroll the message to the bottom
+        messageContainer.scrollTop = messageContainer.scrollHeight;
     }, false);
+
+    //Send message upon pressing Enter
+    document.getElementById('message').addEventListener('keyup', function (pressEnter) {
+        if (pressEnter.keyCode === 13) {
+            pressEnter.preventDefault();
+            document.getElementById('send-message').click();
+        }
+    })
+
     //Call button
     document.getElementById("call").addEventListener('click', function () {
         alert('Calling: ' + peer_id);
         var call = peer.call(peer_id, myStream);
         call.on('stream', function (stream) {
-            peerVideo.srcObject = stream;
+            //Store in global refrence for later use
+            peerStream=stream
+            peerVideo.srcObject = peerStream;//Display peer video
         });
     }, false);
+
+
     //Connect button
     document.getElementById('connect-to-peer-btn').addEventListener('click', function () {
+        //Store in global refrence for later use
         username = document.getElementById('name').value;
         peer_id = document.getElementById('peer_id').value;
+        //If peer_id exist then connect to peer_id while sending Local username
         if (peer_id) {
             conn = peer.connect(peer_id, {
                 metadata: {
                     'username': username
                 }
             });
-
             conn.on('data', handleMessage);
         } else {
             alert('Provide a peer to connect');
             return false;
         }
-
+        //Disapearing the peer id box and display message
         document.getElementById('chat').className = "";
         document.getElementById('connection-form').className += " invisible";
+        document.getElementById('messages-container').className = "messageBox";
+        //Person has joined the chat
     }, false);
+
+    //Reporting error
+    peer.on('error', function (err) {
+        alert('Something went wrong: ' + err);
+        console.error(err);
+    });
 
 }, false);
--- website-server.js
+++ website-server.js
@@ -11,23 +11,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 //https server for localhost testing
-// var privateKey  = fs.readFileSync('certificates/SecureCall.key', 'utf8');
-// var certificate = fs.readFileSync('certificates/SecureCall.crt', 'utf8');
-
-// var credentials = {key: privateKey, cert: certificate};
+var privateKey  = fs.readFileSync('certificates/SecureCall.key', 'utf8');
+var certificate = fs.readFileSync('certificates/SecureCall.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
 //http server
 var LANtesting='0.0.0.0';
 http.createServer(app).listen(PORT,function(){
     console.log('Listening at '+PORT);
 });
 //https server for localhost testing
-// https.createServer(credentials, app).listen(6969,LANtesting,function(){
-//     console.log('https server listening at: 6969');
-// });
-
+https.createServer(credentials, app).listen(6969,LANtesting,function(){
+    console.log('https server listening at: 6969');
+});
+app.use(express.static('public'));
 app.use('/resources', express.static('./public'));
